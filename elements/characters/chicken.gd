42bf8efd6508b95f2996aca71c3469d1eef90ee5
Anton Kuzan <antonkuzan@gmail.com>
2025-12-05 01:54:35 +0300

    Idle, Ready, Timer - Complete Basics
    Added Ready animation. Added Timer. Connect states and timer.
    
    Now, at startup, the game gives the timer a random number of seconds, after which the Iddle state will change to a Ready state

--- elements/characters/chicken.gd
+++ elements/characters/chicken.gd
@@ -1,12 +1,30 @@
 extends Node2D
 
-@onready var idle_animation = $AnimatedSprite2D
-# Called when the node enters the scene tree for the first time.
+var rnd = RandomNumberGenerator.new()
+var chicken_is_ready := false
+
+@onready var animation = $AnimatedSprite2D
+@onready var timer = $Timer
+
 func _ready() -> void:
-	inner_physic_process("idle")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func inner_physic_process(delta):
-	idle_animation.play()
+	rnd.randomize()
+	timer.wait_time = rnd.randf_range(3.0, 7.0)
+	print("Таймер установлен на %s секунд" % $Timer.wait_time)
+	timer.start()
 	
+	_chicken_is_idle()
+
+func _chicken_is_idle():
+	animation.play("idle")
+	
+func _chicken_is_ready():
+	animation.play("ready")
+	chicken_is_ready = true
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void: #subscribed to signal
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("Персонажа нажали!")
+
+func _on_timer_timeout() -> void:
+	_chicken_is_ready()
+	print("Таймер закончился")
