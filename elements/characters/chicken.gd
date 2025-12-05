d1a6c9184c68f24d79e383c9c2824d4e9ecff3c1
Anton Kuzan <antonkuzan@gmail.com>
2025-12-05 19:44:09 +0300

    Enum for state and Eggs Generation
    Added Enum for Chicken State and Eggs generation added

--- elements/characters/chicken.gd
+++ elements/characters/chicken.gd
@@ -1,30 +1,40 @@
 extends Node2D
 
-var rnd = RandomNumberGenerator.new()
-var chicken_is_ready := false
+var rnd := RandomNumberGenerator.new()
+var eggs_generated := 0
+enum ChickenState { IDLE, READY }
+var state: ChickenState = ChickenState.IDLE
 
 @onready var animation = $AnimatedSprite2D
 @onready var timer = $Timer
 
 func _ready() -> void:
 	rnd.randomize()
+	chicken_is_idle()
+	start_random_timer()
+
+func start_random_timer() -> void:
 	timer.wait_time = rnd.randf_range(3.0, 7.0)
 	print("Таймер установлен на %s секунд" % $Timer.wait_time)
 	timer.start()
-	
-	_chicken_is_idle()
 
-func _chicken_is_idle():
+func chicken_is_idle():
+	state = ChickenState.IDLE
 	animation.play("idle")
-	
-func _chicken_is_ready():
+
+func chicken_is_ready():
+	state = ChickenState.READY
 	animation.play("ready")
-	chicken_is_ready = true
+
+func eggs_generation():
+	eggs_generated = rnd.randi_range(1, 5)
+	print("Курочка сгенерировала %s яйца" % eggs_generated)
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void: #subscribed to signal
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		print("Персонажа нажали!")
 
 func _on_timer_timeout() -> void:
-	_chicken_is_ready()
+	chicken_is_ready()
 	print("Таймер закончился")
+	eggs_generation()
