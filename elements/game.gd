ce2c75842d18d9e216bdb499f17923e6f6703a60
Anton Kuzan <antonkuzan@gmail.com>
2025-12-11 21:49:50 +0300

    Tween flight draft
    Started making a tween flight from an egg to a label. I got stuck on the fact that the egg and the label are two different types of containers (UI and Node 2D).

--- elements/game.gd
+++ elements/game.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 @onready var egg_scene = preload("res://elements/characters/egg.tscn")
-@onready var eggs_container = $Eggs
-@onready var custom_cursor = $CustomCursor
+@onready var eggs_container = $eggs
+@onready var custom_cursor = $custom_cursor
+@onready var score_label = $score_label
 
 func _ready() -> void:
 	for chicken in get_tree().get_nodes_in_group("chickens"):
@@ -19,11 +20,19 @@ func on_eggs_spawned(count):
 func on_chicken_was_clicked(chicken):
 	print("Game: получен сигнал chicken_was_clicked")
 	
-	var egg = egg_scene.instantiate()
-	eggs_container.add_child(egg)
-	egg.global_position = chicken.global_position
+	var egg = egg_scene.instantiate() # create an egg
+	eggs_container.add_child(egg) # throw it into the branch
+	egg.global_position = chicken.global_position # put the egg in the chicken's position
 	
 	var tween = get_tree().create_tween()
-	tween.tween_property(egg, "position:y", egg.position.y - 100, 0.8).as_relative()
-	tween.tween_callback(Callable(egg, "queue_free"))
-	tween.play()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.set_ease(Tween.EASE_OUT)
+	
+	var target_pos = score_label.get_global_position()
+	tween.tween_property(egg, "global_position", target_pos, 0.5)
+	tween.finished.connect(egg.queue_free)
+	
+	#var tween_egg_movement = get_tree().create_tween()
+	#tween_egg_movement.tween_property(egg, "position:y", egg.position.y - 100, 0.8).as_relative()
+	#tween_egg_movement.tween_callback(Callable(egg, "queue_free"))
+	#tween_egg_movement.play()
