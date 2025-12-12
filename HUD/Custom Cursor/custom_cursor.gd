14b4a4c00767ffb00ec611235899622d97ba879a
Anton Kuzan <antonkuzan@gmail.com>
2025-12-12 23:44:49 +0300

    New project architecture
    Created a new project architecture. Key points:
    1. Starting with the functionality of in-game elements (at the top level)
    2. Moving to the asset type (at the bottom level)
    
    For example:
    Previously, we used the hierarchy: Elements -> Sprites -> Chicken
    Now, we use the hierarchy: Entities -> Chicken -> Sprites

---
+++ HUD/Custom Cursor/custom_cursor.gd
@@ -0,0 +1,30 @@
+extends TextureRect
+
+var cursor_default = preload("res://HUD/Custom Cursor/Art/cursor_default.png")
+var cursor_hovered = preload("res://HUD/Custom Cursor/Art/cursor_hovered.png")
+var cursor_clicked = preload("res://HUD/Custom Cursor/Art/cursor_clicked.png")
+
+var is_hovered = false
+
+@onready var chicken = preload("res://Entities/Organisms/Chicken/Chicken/chicken.tscn")
+
+func _ready() -> void:
+	Input.set_custom_mouse_cursor(cursor_default,Input.CURSOR_ARROW, Vector2(0, 0))
+	Input.set_custom_mouse_cursor(cursor_hovered,Input.CURSOR_POINTING_HAND, Vector2(0, 0))
+	Input.set_custom_mouse_cursor(cursor_clicked,Input.CURSOR_CAN_DROP, Vector2(0, 0))
+
+func set_default_cursor():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+	is_hovered = false
+
+func set_hover_cursor():
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	is_hovered = true
+
+func set_click_cursor():
+	Input.set_default_cursor_shape(Input.CURSOR_CAN_DROP)
+	await get_tree().create_timer(0.1).timeout
+	if is_hovered:
+		set_hover_cursor()
+	else:
+		set_default_cursor()
---
+++ Stages/BaseStage.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var egg_scene = preload("res://Entities/Organisms/Chicken/Egg/egg.tscn")
+@onready var eggs_container = $eggs
+@onready var custom_cursor = $custom_cursor
+@onready var score_label = $score_label
+
+func _ready() -> void:
+	for chicken in get_tree().get_nodes_in_group("chickens"):
+		chicken.connect("chicken_is_prepared", Callable(self, "on_chicken_is_prepared"))
+		chicken.connect("eggs_spawned", Callable(self, "on_eggs_spawned"))
+		chicken.connect("chicken_was_clicked", Callable(self, "on_chicken_was_clicked"))
+
+func on_chicken_is_prepared():
+	print("Game: получен сигнал chicken_is_prepared")
+
+func on_eggs_spawned(count):
+	print("Game: получен сигнал eggs_spawned =", count)
+
+func on_chicken_was_clicked(chicken):
+	print("Game: получен сигнал chicken_was_clicked")
+	
+	var egg = egg_scene.instantiate() # create an egg
+	eggs_container.add_child(egg) # throw it into the branch
+	egg.global_position = chicken.global_position # put the egg in the chicken's position
+	
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.set_ease(Tween.EASE_OUT)
+	
+	var target_pos = score_label.area2d.get_global_transform_with_canvas().origin
+	tween.tween_property(egg, "global_position", target_pos, 0.5)
+	tween.finished.connect(egg.queue_free)
